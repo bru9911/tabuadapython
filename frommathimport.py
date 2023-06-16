18d83b30d172bb1998dc07c1b9d5e098c184b1f9
Bruno Padovani Canna <60715983+bru9911@users.noreply.github.com>
2023-06-16 11:25:13 -0300

    Create frommathimport.py

---
+++ frommathimport.py
@@ -0,0 +1,6 @@
+#Importando todos os Atributos de um Módulo
+#Você também pode importar todos os atributos de um módulo de uma vez, usando a sintaxe from e import *:
+
+from math import *
+print(sqrt(16)) # saída: 4.0
+#Nesse exemplo, importamos todos os atributos do módulo math. Agora podemos usar a função sqrt diretamente, sem precisar digitar o nome do módulo toda vez. No entanto, essa abordagem não é recomendada, pois pode levar a conflitos de nome e tornar seu código mais difícil de ler e entender.
